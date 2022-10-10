ad4ef7df3b105f79ac43a8c70c3aa271ccd2bcd0
Oluwafumbi Fashanu <fumbi@oluwafumbis-mbp.home>
2022-10-11 00:40:21 +0100

    k

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -124,6 +124,13 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,  'templates'),
+    # Add to this list all the locations containing your static files 
+)
+
 MEDIA_URL = "/images/"
 
 # STATICFILES_DIRS = [

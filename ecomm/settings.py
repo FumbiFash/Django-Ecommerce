cae75e7526464e206156a448f9e497e3f2e780d7
FumbiFash <71007850+FumbiFash@users.noreply.github.com>
2022-10-10 12:25:36 +0100

    Update settings.py

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!bfdrhy)k_+=nz1pp%x2$cj9$=00pq+10r^3awh$kl1@bvve0-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ecommfuntitureapp.herokuapp.com']
+ALLOWED_HOSTS = ['ecommfurnitureapp.herokuapp.com']
 
 
 # Application definition

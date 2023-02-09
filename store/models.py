814ddfeee40d429121bffa3e9ae88755f2a7afc2
Fumbi <fumbifash@ymail.com>
2023-02-09 16:57:45 +0000

    changes

--- store/models.py
+++ store/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import AbstractUser
 
     
 class User(AbstractUser):
-    name =   models.CharField(max_length=200,null = True)
+    name =  models.CharField(max_length=200,null = True)
     email = models.EmailField(max_length=200,unique=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']

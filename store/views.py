49f8188f4609e58563f5de19e8d51f1dae061348
FumbiFash <71007850+FumbiFash@users.noreply.github.com>
2024-02-27 19:02:30 +0000

    Update views.py

--- store/views.py
+++ store/views.py
@@ -37,16 +37,7 @@ def store(request):
     return render(request,"store/shop.html",context)
 
 def cart(request):
-    # customers = Customer.objects.all()
    
-    # orders = Order.objects.all()
-    # orderitems = OrderItem.objects.all()
-    # orderitemsNo = orderitems.count()
-    
-    # totalcost = 0
-    # for i in orderitems:
-    #     totalcost = totalcost + (i.product.price * i.quantity)
-     # context = {"customers":customers,"products":products,"orders":orders,"orderitems":orderitems,"orderitemsNo":orderitemsNo,"totalcost":totalcost}
 
     products = Product.objects.all()
     cartdata = cartData(request)
@@ -54,14 +45,6 @@ def cart(request):
     order = cartdata['order']
     cartitems = cartdata['cartitems']
 
-        
-         # _set.all allows us to do reverse search/query on the model
-        #  _set.all allows is used on foreign key values for reverse query 
-        #  _set.all is used to (reverse) query for that specific object
-    
-        
-    
-    
 
    
     context = {"items":items,"order":order,"cartitems":cartitems}
@@ -126,26 +109,6 @@ def processOrder(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer = customer, complete = False)
-        # total = float(data['userformdata']['total'])
-       
-        # if total == float(order.get_cart_total):
-        #     order.complete = True
-
-        # order.save()    
-
-        # Below line is wrong becuase it says, if shipping address exists, then ammend the address, when i should actually just create a new address for the customer
-        # shippingadd, created = ShippingAddress.objects.get_or_create(customer = customer)
-
-        
-
-        # shippingadd.order = Order.objects.get(customer = customer, complete = False)
-        # shippingadd.address = shippinginfo['address']
-        # shippingadd.city = shippinginfo['city']
-        # shippingadd.state = shippinginfo['state']
-        # shippingadd.save()
-        # print(request.body)
-
-    # print(creditds)
 
     else:
 
@@ -169,28 +132,6 @@ def processOrder(request):
                 state = shippinginfo['state'],
                 zipcode = shippinginfo['zipcode']
             )
-
-        # customer = "Anonymous"
-        # order, created = Order.objects.get_or_create(customer = 'customer', complete = False)
-        # total = float(data['userformdata']['total'])
-       
-        # if total == float(order.get_cart_total):
-        #     order.complete = True
-
-        # order.save()    
-        # order.transaction_id = transaction 
-
-        # shippinginfo = data['shippinginfo']
-
-        # if order.shipping == True:
-        #     ShippingAddress.objects.create(
-        #         customer = customer,
-        #         order = order,
-        #         address = shippinginfo['address'],
-        #         city = shippinginfo['city'],
-        #         state = shippinginfo['state'],
-        #         zipcode = shippinginfo['zipcode']
-        #     )
 
 
     print("request: ", request.COOKIES)
